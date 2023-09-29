e47977e521a9f6ced21396882372d3c5e76eec46
GROUP\UT84218 <gemma.centrone@esterni.credemtel.it>
2023-09-29 17:52:57 +0200

    feat(general)web

--- js/js.js
+++ js/js.js
@@ -17,7 +17,31 @@ $(document).ready(function () {
         });
     })
 })
+
+$(document).ready(function () {
+    $(".on").click(function () {
+        $('.alert').css({
+            'transform': 'translateX(0)',
+        });
+    });
+
+    $(".close").click(function () {
+        $('.alert').css({
+            'transform': 'translateX(150%)',
+        });
+    });
+
+    $(".out").click(function () {
+        $('.alert').css({
+            'transform': 'translateX(150%)',
+        });
+    })
+})
+
+
+
 ScrollReveal({ reset: true });
 ScrollReveal().reveal('.banner', { easing: "ease-in", delay: 100 });
-ScrollReveal().reveal('.shop', { easing: "ease-in", delay: 100 });
-ScrollReveal().reveal('.about', { easing: "ease-in", delay: 100 });
+ScrollReveal().reveal('.shop', { easing: "ease-in", delay: 200 });
+ScrollReveal().reveal('.about', { easing: "ease-in", delay: 200 });
+ScrollReveal().reveal('.contact', { easing: "ease-in", delay: 200 });
